1f1126e1d6d7aa5831d257b7e9cabb437a676e0a
Noah Marriott <noahmm798@gmail.com>
2015-07-19 03:38:43 -0700

    edits 3:38 AM

--- frontend/newsearch.php
+++ frontend/newsearch.php
@@ -9,7 +9,7 @@
 }
 .product {
     width : 360px;
-    height : 500px;
+    height : 475px;
     background-color : #DDDDDD;
     margin : 10px 10px 10px 10px;
     float : left;
@@ -43,6 +43,98 @@
     height: 40px;
     background-color: gray;
 }
+ .product{
+       
+        width: 345px;
+        position: relative;
+        top:5px;
+        background-color: white;
+        height: 600px;
+        border-radius: 5px;
+        left: 15px;
+      }
+      
+      .product img{
+        width:inherit;
+        border-radius: 5px;
+      }
+      .product hr{
+        width:90%;
+        color:#F3F3F3;
+      }
+      .product h1{
+        font-size: 20px;
+        position: relative;
+        left:15px;
+        font-family: 'Helvetica', 'Helvetica-neue';
+        font-weight: bold;
+      }
+     .product p{
+         font-size: 15px;
+         font-weight: 400;
+         margin-top:-3px;
+        position: relative;
+        left:15px;
+        font-family: 'Helvetica', 'Helvetica-neue';
+        
+       
+       
+     }
+     
+    
+     button{
+        float: left;
+        width: 325px;
+
+        height: 40px;
+        border: 0px;
+        background-color: #F67400;
+      margin-left:10px;
+      margin-bottom: 10px;
+      color: white;
+      border-radius: 5px;
+      font-family: myriad-pro,"Helvetica Neue",Helvetica,Arial,sans-serif;
+       font-weight:bold;
+     font-size:18px;
+     }
+    
+    h3{
+        
+         font-family: 'Helvetica', 'Helvetica-neue';
+        font-weight: 700;
+         
+         margin-top:20px;
+         margin-left:13px;
+         top: 7px;
+position: relative;
+     }
+     .sec{
+        color: #CC3636;
+     }
+     .precent{
+        color: red;
+     }
+     h5{
+     font-size: 20px;
+     margin: 5px;
+      font-family: 'Helvetica', 'Helvetica-neue';
+        font-weight: 700;
+        margin-top:-12px;
+         margin-left:13px;
+     }
+     h4{
+      font-family: 'Helvetica', 'Helvetica-neue';
+        margin-top: 5px;
+       font-size: 20px;
+         margin-left:13px;
+         font-weight:400;      
+     }
+     .wrap-products{
+        width:1000px;
+
+        margin: 0 auto;
+        float: left;
+     }
 
         </style>
     </head>
@@ -58,16 +150,52 @@
             You Searched For Apple iPad
         </div>
             <div class="product">
-                Product
+                <img src="images/beats.png">
+        <hr size="1">
+        <h1 > <?php // echo name?>Beats Solo Premium</h1>
+        <p>Starting Discount <?php // echo precent?>5% MSRP</p>
+        
+       
+        <button>See Product</button><h3>Ends: <?php// echo time?>1 Day 18 Min <span class="sec">34 Sec</span></h3>
+        <h4>Current Discount <?php // echo precenatge?> <span class="precent">Save 15%</span></h4>
+        <h5><?php// echo number sold?> 200 Sold Of 500</h5>
+       
             </div>
             <div class="product">
-                Product
+                <img src="images/beats.png">
+        <hr size="1">
+        <h1 > <?php // echo name?>Beats Solo Premium</h1>
+        <p>Starting Discount <?php // echo precent?>5% MSRP</p>
+        
+       
+        <button>See Product</button><h3>Ends: <?php// echo time?>1 Day 18 Min <span class="sec">34 Sec</span></h3><div class="clear"></div>
+        <h4>Current Discount <?php // echo precenatge?> <span class="precent">Save 15%</span></h4>
+        <h5><?php// echo number sold?> 200 Sold Of 500</h5>
+       
             </div>
             <div class="product">
-                Product
+                <img src="images/beats.png">
+        <hr size="1">
+        <h1 > <?php // echo name?>Beats Solo Premium</h1>
+        <p>Starting Discount <?php // echo precent?>5% MSRP</p>
+        
+       
+        <button>See Product</button><h3>Ends: <?php// echo time?>1 Day 18 Min <span class="sec">34 Sec</span></h3><div class="clear"></div>
+        <h4>Current Discount <?php // echo precenatge?> <span class="precent">Save 15%</span></h4>
+        <h5><?php// echo number sold?> 200 Sold Of 500</h5>
+       
             </div>
             <div class="product">
-                Product
+                <img src="images/beats.png">
+        <hr size="1">
+        <h1 > <?php // echo name?>Beats Solo Premium</h1>
+        <p>Starting Discount <?php // echo precent?>5% MSRP</p>
+        
+       
+        <button>See Product</button><h3>Ends: <?php// echo time?>1 Day 18 Min <span class="sec">34 Sec</span></h3><div class="clear"></div>
+        <h4>Current Discount <?php // echo precenatge?> <span class="precent">Save 15%</span></h4>
+        <h5><?php// echo number sold?> 200 Sold Of 500</h5>
+       
             </div>
         </div>
         <div class="sponsor-ad">
